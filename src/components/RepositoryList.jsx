2ab18f37c2de675a9eaab3dc9f56c25c82b45cf5
GilbertoASJ <alvesgilberto84@gmail.com>
2021-03-11 17:21:58 -0300

    Creating system for api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
+
+// https://api.github.com/users/GilbertoASJ/repos
 
 const repository = {
 	name: 'unform',
@@ -8,11 +11,22 @@ const repository = {
 }
 
 export function RepositoryList() {
+	const [repositories, setRepositories] = useState([])
+
+	// useEffect recebe dois parâmetros, a função que irá executar, e quando executar essa função
+	useEffect(() => {
+		fetch('https://api.github.com/users/GilbertoASJ/repos')
+		 .then(response => response.json())
+		 .then(data => setRepositories(data))
+
+	}, [])
+
 	return (
 		<section className="repository-list">
 			<h1>Lista de repositórios</h1>
 
 			<ul>
+				<RepositoryItem repository={repository} />
 				<RepositoryItem repository={repository} />
 				<RepositoryItem repository={repository} />
 				<RepositoryItem repository={repository} />
