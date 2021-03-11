ee196fc8eefc1c6fafe169e26a9abd977114a8a8
GilbertoASJ <alvesgilberto84@gmail.com>
2021-03-11 16:15:48 -0300

    Adding ReactRefreshWebpackPlugin to project

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,19 @@
+// Importando useState, função do react que permite utilizar o conceito de estado
 import { useState } from 'react'
+
+// O conceito da imutabilidade em React, consiste em não sobrepor variáveis, não sobrescrever seus valores, e sim criar uma nova variável e atribuir o valor da antiga por exemplo:
+
+/*
+
+let usuarios = ['Gilberto', 'Júnior', 'Scy']
+
+No javascript condicional seria normal fazer: usuarios.push('newUser'), porém no React, com o conceito da imutabilidade, o mais convecional a se fazer é:
+
+let novoUsuarios = [...usuarios, 'newUser']
+
+Fazendo isso ao invés de sobrescrever a antiga variável, criamos uma nova atribuindo seu antigo valor, adicionando um novo valor.
+
+*/
 
 export function Counter() {
 	const [counter, setCounter] = useState(0);
@@ -10,7 +25,7 @@ export function Counter() {
 	return (
 		<div>
 			<h2>{counter}</h2>
-			<button type="button" onClick={increment}>Increment</button>
+			<button type="button" onClick={increment}>Increment +1</button>
 		</div>
 	)
 }
