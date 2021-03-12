695015d9cd18a9312656866cc46d159ebeab741e
GilbertoASJ <alvesgilberto84@gmail.com>
2021-03-12 15:22:57 -0300

    Updating files

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,11 +4,7 @@ import '../styles/repositories.scss'
 
 // https://api.github.com/users/GilbertoASJ/repos
 
-const repository = {
-	name: 'unform',
-	description: 'Forms in React',
-	link: 'https://github.com'
-}
+
 
 export function RepositoryList() {
 	const [repositories, setRepositories] = useState([])
@@ -26,11 +22,10 @@ export function RepositoryList() {
 			<h1>Lista de repositórios</h1>
 
 			<ul>
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
+				{repositories.map(repository => {
+					return <RepositoryItem key={repository.name} repository={repository} />
+				})}
+				
 			</ul>
 		</section>
 	)
